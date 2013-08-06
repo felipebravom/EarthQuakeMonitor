99618312da36a60e392213ff9abf7ba13343cd6e
fbravo <felipebravom@gmail.com>
2013-08-06 18:48:02 -0400

    commit

---
+++ src/topicTracker/Constant_Settings.java
@@ -0,0 +1,11 @@
+package topicTracker;
+
+public class Constant_Settings {
+	public final static String OAUTH_CONSUMER_KEY= "2J6YxWjj7zaVt979uoZtA";
+	public final static String OAUTH_CONSUMER_SECRET= "8cIMS0nopUvQ8IVQZIUAx1SE2F56YoIC4PtcEDjn9E";
+	
+	public final static String OAUTH_ACCESS_TOKEN= "145084142-F54lBJdshyuLHf43ROpsUqzYt2NIbVqewjLqVdDu";
+
+	public final static String OAUTH_ACCESS_TOKEN_SECRET= "XKCKw6YkZknPXR9A1PgjjiJzQf0MkWBIsz2pobN3VI";
+	
+}
---
+++ src/topicTracker/Prueba.java
@@ -0,0 +1,11 @@
+package topicTracker;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola MUNDO");
+
+	}
+
+}
