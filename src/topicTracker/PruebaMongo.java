9f6f140253e4e607998bb8ed28ee76903867ba17
fbravo <felipebravom@gmail.com>
2013-08-08 19:20:10 -0400

    Mejoras a consultas

--- src/topicTracker/PruebaMongo.java
+++ src/topicTracker/PruebaMongo.java
@@ -32,68 +32,114 @@ public class PruebaMongo {
 	public final static String OAUTH_ACCESS_TOKEN_SECRET= "XKCKw6YkZknPXR9A1PgjjiJzQf0MkWBIsz2pobN3VI";
 
 
+	private MongoClient mongo;
+	private DB db;
+	private DBCollection table;
+	
+	public void setupMongo() {
+		try {
+			this.mongo = new MongoClient("localhost", 27017);
+			this.db = this.mongo.getDB("EarthQuake");
 
-	public static void main(String[] args) throws UnknownHostException, TwitterException {
-		// TODO Auto-generated method stub
-
-
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setJSONStoreEnabled(true);
-
-		cb.setDebugEnabled(true)
-		.setOAuthConsumerKey(OAUTH_CONSUMER_KEY)
-		.setOAuthConsumerSecret(OAUTH_CONSUMER_SECRET)
-		.setOAuthAccessToken(OAUTH_ACCESS_TOKEN)
-		.setOAuthAccessTokenSecret(OAUTH_ACCESS_TOKEN_SECRET);
-		TwitterFactory tf = new TwitterFactory(cb.build());
-		Twitter twitter = tf.getInstance();
-
-
-
-
-		MongoClient mongo = new MongoClient("localhost", 27017);
-		DB db = mongo.getDB("testdb");
-
-		/**** Get collection / table from 'testdb' ****/
-		// if collection doesn't exists, MongoDB will create it for you
-		DBCollection table = db.getCollection("user");
-
-
-
-		Query query = new Query("temblor OR terremoto OR sismo");
-		query.setCount(100);
-
-		QueryResult result = twitter.search(query);
-		for (Status status : result.getTweets()) {
-
-			GeoLocation geo=status.getGeoLocation();
-
-			if(geo!=null){
-				System.out.println(geo.toString());
-				System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-
-				String json = DataObjectFactory.getRawJSON(status);
-
-				DBObject dbObject = (DBObject)JSON.parse(json);
-
-				table.insert(dbObject);
-
-
-
-			}
-
+			/**** Get collection / table from 'testdb' ****/
+			// if collection doesn't exists, MongoDB will create it for you
+			this.table = db.getCollection("tweet");
+			
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
 		}
 
 
+    }
+	
+	
+	public void saveTweets() throws TwitterException{
+		
+		// Check Mongo Connection
+	
+
+			ConfigurationBuilder cb = new ConfigurationBuilder();
+			cb.setJSONStoreEnabled(true);
+
+			cb.setDebugEnabled(true)
+			.setOAuthConsumerKey(OAUTH_CONSUMER_KEY)
+			.setOAuthConsumerSecret(OAUTH_CONSUMER_SECRET)
+			.setOAuthAccessToken(OAUTH_ACCESS_TOKEN)
+			.setOAuthAccessTokenSecret(OAUTH_ACCESS_TOKEN_SECRET);
+			TwitterFactory tf = new TwitterFactory(cb.build());
+			Twitter twitter = tf.getInstance();
+
+			Query query = new Query("temblor OR terremoto OR sismo OR tsunami");
+			query.setCount(100);
+			QueryResult result = twitter.search(query);
+			for (Status status : result.getTweets()) {
+
+				GeoLocation geo=status.getGeoLocation();
+
+				if(geo!=null){
+					System.out.println(geo.toString());
+					System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+
+					//					geo.getLatitude();
+					//					geo.getLongitude();
+					//					
+					//					status.getUser();
+					//					status.getId();
+					//					status.getText();
+					//					status.getCreatedAt();
+					//					
+					//					
+					//					BasicDBObject tweet = new BasicDBObject("tweetId", status.getId()).
+					//							append("user", status.getUser()).
+					//							append("count", 1).
+					//							append("loc", new BasicDBObject("x", 203).append("y", 102));
+					//
+					//					coll.insert(doc);
+
+
+					String json = "{tweetId:" + status.getId() + ",userId:" + status.getUser().getId() + ",text: \"" + status.getText() + "\",loc: [ " + geo.getLatitude() + ", " + geo.getLongitude() + "] }";
+
+					System.out.println(json);
+
+					//String json = DataObjectFactory.getRawJSON(status);
+
+
+					DBObject dbObject = (DBObject)JSON.parse(json);
+
+					this.table.insert(dbObject);
 
 
 
+				}
 
 
+			
+		}
+			
+		}
+		
+	
 
-
-
-
+	public static void main(String[] args) throws TwitterException  {
+		
+		PruebaMongo pb=new PruebaMongo();
+		pb.setupMongo();
+		pb.saveTweets();
+		
+		
+		
+		
 	}
+	
+	
+		
+		
+
+
+
 
 }
